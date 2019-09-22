39b6e9447bf2000aef1e880fa8602b701db85f6d
Steve Henty <stevehenty@gmail.com>
2019-09-22 17:28:22 +0200

    Extend test

--- tests/acceptance/testHelloCept.php
+++ tests/acceptance/testHelloCept.php
@@ -1,11 +1,16 @@
 <?php
 
+/** @var \Codeception\Scenario $scenario */
 $I = new AcceptanceTester( $scenario );
 
 $I->wantTo( 'Test that Hello Codeception works.' );
 
-$I->logOut();
-
 $I->amOnPage( '/' );
 
 $I->see( 'Hello Codeception' );
+
+$I->click( 'Hello Codeception' );
+
+$I->see( 'Welcome' );
+
+
