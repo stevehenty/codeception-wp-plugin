964008a4dc33a056cfb62766ee2fb0b918a8ef98
Steve Henty <stevehenty@gmail.com>
2019-09-29 13:12:55 +0200

    Revert "Update _bootstrap.php"
    This reverts commit 2537be2f0618556150cbda4f7150c1eb546eac60.

--- tests/acceptance/_bootstrap.php
+++ tests/acceptance/_bootstrap.php
@@ -1,21 +1,25 @@
 <?php
 
+global $wp_rewrite;
+$wp_rewrite->set_permalink_structure( '/%postname%/' );
+$wp_rewrite->flush_rules();
+
 /**
- * Creates a new page with the given slug and content.
+ * Creates a new post with the given slug and content.
  *
- * @param string $name The page name
+ * @param string $name The post name
  *
  * @param string $content
  *
  * @return int|string|WP_Error
  */
-function create_page( $name, $content ) {
+function create_post( $name, $content ) {
 	$post = array(
 		'post_title'   => $name,
 		'post_content' => $content,
 		'post_excerpt' => $name,
 		'post_status'  => 'publish',
-		'post_type'    => 'page',
+		'post_type'    => 'post',
 	);
 
 	$post_id = wp_insert_post( $post );
@@ -23,5 +27,5 @@ function create_page( $name, $content ) {
 	return $post_id ? $post_id : '';
 }
 
-create_page( 'hello', 'Welcome' );
-create_page( 'bye', 'Goodbye' );
+create_post( 'hello', 'Welcome' );
+create_post( 'bye', 'Goodbye' );
